b5b59348394bc0ea77204eee41441fd5a1a17ae5
paulabitencourt <ana.paula.bitencourt14@gmail.com>
2022-02-20 21:14:08 -0300

    adicionando estrutura de repeticao na questao 02

--- src/Questao02.java
+++ src/Questao02.java
@@ -2,10 +2,18 @@ import javax.swing.*;
 
 public class Questao02 {
     public static void main(String[] args) {
-        String senha = JOptionPane.showInputDialog("Digite sua senha:");
-        if (senha.length() < 6){
-            System.out.println("Digite mais " + (6 - senha.length()) + " caractere para sua senha se tornar segura");
+        boolean segura = false;
+        while (segura == false){
+            String senha = JOptionPane.showInputDialog("Digite sua senha:");
+            if (senha.length() < 6){
+                System.out.println("Digite mais " + (6 - senha.length()) + " caractere para sua senha se tornar segura");
+                segura = false;
+            }
+            else{
+                System.out.println("Sua senha é segura!");
+                segura = true;
+            }
         }
+
     }
 }
-
