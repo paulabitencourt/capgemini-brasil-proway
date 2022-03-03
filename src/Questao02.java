55170712b599f80d336192a3ce88fcbffc0a4337
paulabitencourt <ana.paula.bitencourt14@gmail.com>
2022-03-02 22:47:38 -0300

    Adicionando Questao02 do desafio 02

--- src/Questao02.java
+++ src/Questao02.java
@@ -1,19 +1,41 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Questao02 {
     public static void main(String[] args) {
-        boolean segura = false;
-        while (segura == false){
-            String senha = JOptionPane.showInputDialog("Digite sua senha:");
-            if (senha.length() < 6){
-                System.out.println("Digite mais " + (6 - senha.length()) + " caractere para sua senha se tornar segura");
-                segura = false;
-            }
-            else{
-                System.out.println("Sua senha é segura!");
-                segura = true;
+
+        ArrayList<Integer> arrayListNumeros = new ArrayList();
+
+        int contador = 0, loopingArrayList = 0, valorDiferenca, valorTotalDeNumerosDiferentes = 0, numeroArrayList = 0;
+
+        while (loopingArrayList == 0) {
+
+            numeroArrayList = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor para adicionar a lista:"));
+
+            arrayListNumeros.add(numeroArrayList);
+            contador++;
+
+            loopingArrayList = Integer.parseInt(JOptionPane.showInputDialog("Para parar de inserir dados na lista, digite 1"));
+        }
+
+        valorDiferenca = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor para saber a diferença comparado aos valores anteriores: "));
+
+        for (int c = 0; c < arrayListNumeros.size(); c++) {
+
+            int valor = arrayListNumeros.get(c);
+
+            for (Integer array1 : arrayListNumeros) {
+
+                if (valor == (array1 - valorDiferenca)) {
+                    valorTotalDeNumerosDiferentes++;
+                    System.out.println("VALORES QUE TEM COMO DIFERENÇA " + valorDiferenca + " são " + array1 + " e " + valor);
+                }
             }
         }
 
+        System.out.println("O total de valores que são da diferença é " + valorTotalDeNumerosDiferentes);
+
     }
-}
+
+    }
+
